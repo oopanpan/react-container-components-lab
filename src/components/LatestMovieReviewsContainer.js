377fa1a2c2773dc764bed46b94dc1468a74a724c
oopanpan <oopanpan@gmail.com>
2021-03-20 23:57:48 -0700

    commit tree 550d04b6ed3f64b707b806432fc8c8cd9f6f9dcc

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,26 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+export class LatestMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+    }
+
+    componentDidMount(){
+        fetch(URL)
+        .then(r => r.json())
+        .then(reviews => this.setState({reviews: reviews.results}))
+    }
+
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer
